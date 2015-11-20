26506ecc5e3cd605a02040bf578bd3197809e6ef
Hingee <hin117@csiro.au>
2015-11-20 15:13:28 +0800

    covarianceMapEst() gives a map of covariances calculated up to certain distances. (NAs those vectors which require area estimation that is less than 10% of total window area)

--- covEst.R
+++ covEst.R
@@ -18,3 +18,31 @@ covarianceEst <- function(Xi,boundary,v){
   
   return(list(numerator = numerator, denominator = denominator, covarianceEst = covarianceEst))
 }
+
+
+
+#estimate a map of covariance for each vector - pretty naive computationally Xi in OWIN, boundary in OWIN, maxshifts in units the same as Xi 
+#ignores point estimates that use an area smaller than 10% of the window
+covarianceMapEst <- function(Xi,boundary,maxXshiftdistance,maxYshiftdistance){
+  windowArea <- area.owin(boundary)
+  #create the vectors for testing
+  shiftVectorX <- c(-rev(seq(0,maxXshiftdistance,by=Xi$xstep)),seq(Xi$xstep,maxXshiftdistance,by=Xi$xstep))
+  shiftVectorY <- c(-rev(seq(0,maxYshiftdistance,by=Xi$ystep)),seq(Xi$ystep,maxYshiftdistance,by=Xi$ystep))
+  covarMap = matrix(nrow=length(shiftVectorY),ncol=length(shiftVectorX))
+  numeratorMap = matrix(nrow=length(shiftVectorY),ncol=length(shiftVectorX))
+  denominatorMap = matrix(nrow=length(shiftVectorY),ncol=length(shiftVectorX))
+  for (i in 1:length(shiftVectorX)){
+    for (j in 1:length(shiftVectorY)){
+      covarianceEstimation <- covarianceEst(XiOWIN,boundaryOWIN,c(shiftVectorX[i],shiftVectorY[j]))
+      if (is.na(covarianceEstimation$denominator) || (covarianceEstimation$denominator < 0.1*windowArea)){
+        covarMap[i,j] <- NA
+      }
+      else {covarMap[i,j] <- covarianceEstimation$covarianceEst}
+      numeratorMap[i,j] <- covarianceEstimation$numerator
+      denominatorMap[i,j] <- covarianceEstimation$denominator
+    }
+  } 
+  
+  covarianceMap = list(covariance = covarMap, numerator=numeratorMap, denominator = denominatorMap, xcol = shiftVectorX, ycol = shiftVectorY, xstep=Xi$xstep,ystep=Xi$ystep)
+  return(covarianceMap) 
+}
