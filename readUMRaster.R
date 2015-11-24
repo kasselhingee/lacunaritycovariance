69f2f40359dfa1da4c45644f702e80d7389bb877
Kassel Hingee <hinkass@gmail.com>
2015-11-24 16:31:38 +0800

    turned spTransform test into a function. Don't think maptools is required anymore but haven't tested it

--- readUMRaster.R
+++ readUMRaster.R
@@ -1,57 +1,26 @@
 #given a polygon, function works out which UM tile is needed and returns raster data
 
 require(raster)
-require(maptools)
+#require(maptools)
 require(rgdal)
-#scratch stuff
-#polygon with same projection as UM data
-polyOGR <- readOGR("data","poly03") #works if GDA94 / MGA 50 are selected as the CRS when creating layer in QGIS
-crs(proj4string(polyOGR))
-polygonMAPTOOLS <- readShapeSpatial("data/poly03.shp",proj4string = crs(proj4string(polyOGR)))
 
-#same polygon with different projection to UM data
-polyOGR <- readOGR("data","poly03_albers") #in CRS GDA94 / australian albers
-crs(proj4string(polyOGR))
-polygonMAPTOOLS <- readShapeSpatial("data/poly03_albers.shp",proj4string = crs(proj4string(polyOGR)))
-
-
-proj4string(polygonMAPTOOLS) == "+proj=utm +zone=50 +south +ellps=GRS80 +units=m +no_defs"
-
-
-mspImage <- readUMraster(polygonMAPTOOLS,"dom","C:/CCI-02_Work/processing_102/UM2009/")
-treMask <- readUMraster(polygonMAPTOOLS,"tre","C:/CCI-02_Work/processing_102/UM2009/")
-
-plot(mspImage)
-
-"+proj=utm +zone=50 +south +ellps=GRS80 +units=m +no_defs"
-
-plot(add=TRUE,spTransform(polygonMAPTOOLS,CRS("+proj=utm +zone=50 +south +ellps=GRS80 +units=m +no_defs")))
-
-#####Numerical test of spTransform
-layout(matrix(c(1,2),ncol=2))
-#polygon with same projection as UM data
-polyOGR_MGA50 <- readOGR("data","poly03") 
-coords_MGA50 <- polyOGR_MGA50@polygons[[1]]@Polygons[[1]]@coords  #an array of the coordinates of the polgon
-
-
-#same polygon with different projection 
-polyOGR_albers <- readOGR("data","poly03_albers") #in CRS GDA94 / australian albers
-#obsolete: readOGR loads polygon too :) polygon_albers <- readShapeSpatial("data/poly03_albers.shp",proj4string = crs(proj4string(polyOGR_albers)))
-coords_albers <- polyOGR_albers@polygons[[1]]@Polygons[[1]]@coords  #an array of the coordinates of the polgon
-polygon_albers_trans <- spTransform(polyOGR_albers,CRS("+proj=utm +zone=50 +south +ellps=GRS80 +units=m +no_defs")) #function from rgdal
-coords_albers_trans <- polygon_albers_trans@polygons[[1]]@Polygons[[1]]@coords #an array of the coordinates of the polgon
-
-#after transformation these should be the same
-abs(coords_albers_trans - coords_MGA50) < 1e-7
+#test rgdal's spTransform using two versions of poly03 in different CRS
+test_reprojection <- function(){
+  #polygon with same projection as UM data
+  polyOGR_MGA50 <- readOGR("data","poly03") 
+  coords_MGA50 <- polyOGR_MGA50@polygons[[1]]@Polygons[[1]]@coords  #an array of the coordinates of the polgon
   
   
+  #same polygon with different projection 
+  polyOGR_albers <- readOGR("data","poly03_albers") #in CRS GDA94 / australian albers
+  #obsolete: readOGR loads polygon too :) polygon_albers <- readShapeSpatial("data/poly03_albers.shp",proj4string = crs(proj4string(polyOGR_albers)))
+  coords_albers <- polyOGR_albers@polygons[[1]]@Polygons[[1]]@coords  #an array of the coordinates of the polgon
+  polygon_albers_trans <- spTransform(polyOGR_albers,CRS("+proj=utm +zone=50 +south +ellps=GRS80 +units=m +no_defs")) #function from rgdal
+  coords_albers_trans <- polygon_albers_trans@polygons[[1]]@Polygons[[1]]@coords #an array of the coordinates of the polgon
   
-polygon_albers == polygon_MGA50
-plot(polyOGR_MGA50,col ="red")
-plot(add=TRUE,polygon_albers_trans,col ="green")
-
-proj4string(polygonMAPTOOLS) == "+proj=utm +zone=50 +south +ellps=GRS80 +units=m +no_defs"
-
+  #after transformation these should be the same
+  return(abs(coords_albers_trans - coords_MGA50) < 1e-7)
+}
 
 
 #actual function
