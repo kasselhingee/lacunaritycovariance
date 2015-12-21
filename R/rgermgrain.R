70c7bb5eda10868f775ec908ec1d2af5db9b7d3d
HingeeLaptop <hinkass@gmail.com>
2015-12-21 18:25:27 +1100

    placegerms function now has check for no points existing

--- R/rgermgrain.R
+++ R/rgermgrain.R
@@ -3,6 +3,10 @@
 #randomly places grains with from a library of grains
 #grainlib must a solist, pp a point pattern
 placegrainsfromlib <- function(pp,grainlib,replace=TRUE,prob=NULL){
+  if (pp$n == 0){
+    warning("there were no points in the point process - returning empty window")
+    return(NULL)
+  }
   grains <- sample(grainlib,size=pp$n,replace=replace,prob=prob) 
   pointlocations <- cbind(X=pp$x,Y=pp$y)
   pointlocations <- split(cbind(pointlocations),row(pointlocations)) #split matrix into a list of the rows
