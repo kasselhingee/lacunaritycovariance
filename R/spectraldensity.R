75617ef4ce14bfed5175642d1dd59dc964aef8f8
HingeeLaptop <hinkass@gmail.com>
2015-12-23 15:26:53 +1100

    Spectral Density functions added, still not tested. Help file created using roxygen2

--- R/spectraldensity.R
+++ R/spectraldensity.R
@@ -1,22 +1,82 @@
 #'Spectral Density of a RACS
-
+#' @aliases unsmoothedspectraldensity
+#' 
 #' @description 
-#' \code{spectraldensity} estimates the spectral density of a RACS without any kernal smoothing.
+#' \code{unsmoothedspectraldensity} estimates the spectral density of a RACS without any kernal smoothing.
+#' \code{spectraldensity} smoothes the estimate and is my recommended function due to known asymptotoic properties [1].
 #' 
 #' @details 
 #' Applies FFT to the input image and takes the square of the magnitude to estimate the (non-kernel smoothed) spectral density
 #' as described in [B\"{o}hm 2004] (the very first equation or equivalently \eqn{\hat{h}(x)} of equation 4.6 in the 2002 report).  
 #' 
+#' \eqn{\hat{h}(z) := \frac{1}{|w|}\left|\int_{R^2}1_w(x) (1_\Xi(x)-p) dx\right|}
+#' 
+#' This is what \code{unsmoothedspectraldensity} returns.
+#'  \code{spectraldensity} then kernel smooths \eqn{\hat{h}} before returning a spectral density estimate.
 #' 
 #' @references Böhm, S., Heinrich, L., Schmidt, V., 2004. Kernel Estimation of the Spectral Density of Stationary Random Closed Sets. Australian & New Zealand Journal of Statistics 46, 41–51. doi:10.1111/j.1467-842X.2004.00310.x
 #' 
 #' 
-#' @param X A rectangular observation of \eqn{Xi}. NA's are assumed to mean outside \eqn{\Xi} rather than missing data or anything (**I haven't assesed the theory/computations for non-rectangular windows but its probably the same)
+#' @param Xi A rectangular observation of \eqn{Xi}. NA's are assumed to mean outside \eqn{\Xi} rather than missing data or anything (**I haven't assesed the theory/computations for non-rectangular windows but its probably the same)
 #' @param w Observation window (must be rectangular for now)
-#' @param ... Arguments passed to \code{as.mask} to convert observation into a pixel image
-#steal ideas from spatstat's convolve.im
+#' @param bandwidth Bandwidth for the chosen kernel (in reciprocal of the units of \code{Xi})
+#' @param kernel Specifies the kernel to use. Currently only Epanechnikov is supported (and is the default).
+#' @param ... Arguments passed to \code{as.mask} to convert observation into a pixel image. This is useful for specifying the resolution of the raster derived from \code{Xi} (otherwise the raster will always have 128x128 pixels).
+#' 
+#' 
+#' 
+#' @section WARNING: WARNING RESULTS OF THIS FUNCTION HAVENT BEEN TESTED - THEY COULD BE WILDLY WRONG
+#' 
+#' Also final result should have odd pixel dimensions (so that the spectral density at origin is easy to pull out), but convolve.im returns even dimensions, I don't know why yet.
+#' 
 
-spectraldensity <- function(Xi,w,...){
+#' @examples 
+#' #apply to heather
+#' xi <- heather$coarse
+#' specdens <- spectraldensity(xi,Frame(xi),20)
+#' plot(specdens)
+#' 
+#' #See what unsmoothed version looks like
+#' unsmspecdens <- unsmoothedspectraldensity(xi,Frame(xi))
+#' plot(unsmspecdens)
+#' plot(solist(smoothed = specdens,unsmoothed = unsmspecdens),
+#'        main = "Spectral Density Estimates",
+#'        equal.scales=TRUE)
+#' 
+
+
+
+spectraldensity <- function(Xi,w,bandwidth,kernel="Epanechnikov",...){
+  specdens <- unsmoothedspectraldensity(Xi,w,...)
+  xstep = specdens$xstep
+  ystep = specdens$ystep
+  if (kernel == "Epanechnikov") {supportwidth = bandwidth}
+  else {
+    warning("In spectral density smoothing support width of kernel is unknown, defaulting to 3x bandwidth")
+    supportwidth = 3*bandwidth
+    }
+  X <- seq(0,supportwidth*1.5+xstep,by=xstep) #much larger than support width to avoid boundary issues?
+  Y <- seq(0,supportwidth*1.5+ystep,by=ystep)
+  if (kernel == "Epanechnikov"){
+    mat <- outer(X/bandwidth,Y/bandwidth,FUN="EpanechnikovFcn") #rows correspond to xstep - just a quirk of outer!
+    mat <- t(mat) #columns correspond to changes in X, rows correspond to changes in Y!
+    #reflect out to all corners
+    mat <- mat[,c((ncol(mat)):2,1:ncol(mat))]
+    mat <- mat[c((nrow(mat)):2,1:nrow(mat)),]
+    kernelfcn <- im(mat,xcol=c(-X[length(X):2],X),yrow=c(-Y[length(Y):2],Y))
+  }
+  else {
+    stop("Given kernel is not supported for spectral density smoothing")
+  }
+  #apply kernel using convolve.im
+  smspecdens <- convolve.im(specdens,kernelfcn)
+  smspecdens <- smspecdens[Frame(specdens)]
+  return(smspecdens)
+}
+
+
+#steal ideas from spatstat's convolve.im
+unsmoothedspectraldensity <- function(Xi,w,...){
   stopifnot(is.owin(Xi))
   stopifnot(is.rectangle(w))
   Xi <- intersect.owin(Xi,w)
@@ -56,4 +116,13 @@ spectraldensity <- function(Xi,w,...){
   
   return(specdens)
   
+}
+
+EpanechnikovFcn <- function(X,Y){#WARNING: operates in 2D only on a vector of things 
+  stopifnot(length(X)==length(Y))
+  result <- vector(length=length(X),mode="numeric")
+  sz <- sqrt((X*X)+(Y*Y))
+  result[sz>1] <- 0
+  result[sz<=1] <- 2/pi*(1-sz[sz<=1]^2)
+  return(result)
 }
