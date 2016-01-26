af855412ce3c2e0f331f2624b5dc8f46001584f8
Kassel Hingee <hinkass@gmail.com>
2016-01-26 09:59:29 +0800

    spectral density must take pixel masks, a few bugs fixed too

--- stationaryracsinference/R/spectraldensity.R
+++ stationaryracsinference/R/spectraldensity.R
@@ -17,7 +17,7 @@
 #' @references B\"{o}hm, S., Heinrich, L., Schmidt, V., 2004. Kernel Estimation of the Spectral Density of Stationary Random Closed Sets. Australian & New Zealand Journal of Statistics 46, 41--51. doi:10.1111/j.1467-842X.2004.00310.x
 #' 
 #' 
-#' @param Xi A rectangular observation of \eqn{Xi}. NA's are assumed to mean outside \eqn{\Xi} rather than missing data or anything (**I haven't assesed the theory/computations for non-rectangular windows but its probably the same)
+#' @param Xi A rectangular observation of \eqn{Xi}. NA's are assumed to mean outside \eqn{\Xi} rather than missing data. Xi must be pixel mask owin object. (**I haven't assesed the theory/computations for non-rectangular windows but its probably the same)
 #' @param w Observation window (must be rectangular for now)
 #' @param bandwidth Bandwidth for the chosen kernel (in reciprocal of the units of \code{Xi})
 #' @param kernel Specifies the kernel to use. Currently only Epanechnikov is supported (and is the default).
@@ -78,27 +78,43 @@ spectraldensity <- function(Xi,w,bandwidth,kernel="Epanechnikov",...){
 
 #steal ideas from spatstat's convolve.im
 #' @rdname spectraldensity
-unsmoothedspectraldensity <- function(Xi,w,...){
+#' @param suffspecres Optional. A desired spectral resolution, the output resolution will be smaller than \code{suffspecres}
+#'  If it is not provided then resolution will be 2*pi/(size of spatial extent of window), for example a window 500m x 500m will result in spectral resolution of 2*pi/500 in both X and Y directions.
+#'  Can be of length 1 (applies to all dimensions), or length 2 (a differen desired resolution for each dimension).
+unsmoothedspectraldensity <- function(Xi,w,suffspecres=NULL,...){
   stopifnot(is.owin(Xi))
-  stopifnot(is.rectangle(w))
+  stopifnot(is.mask(Xi))
+  stopifnot(is.rectangle(w)) #because theory uses rectangular windows, I'm going to assume a rectangular window to - maybe improve on this later
   Xi <- intersect.owin(Xi,w)
   p <- covpest(Xi,w)
-  M <- as.im(Xi,...)
-  xstep=M$xstep
-  ystep=M$ystep
-  M <- as.matrix(M)
-  M[is.na(M)] <- 0 #since the function that we wish to transform is an indicator of both inside window, and inside xi. Its ok to set all NAs to 0
+  xstep=Xi$xstep
+  ystep=Xi$ystep
+  M <- as.matrix(Xi)
+  M[is.na(M)] <- 0 #since the function that we wish to transform is an indicator of inside window AND inside xi. Its ok to set all NAs to 0
   M <- M-p
-  fM <- xstep*ystep*fft(M) #xstep*ystep = scale to approximate Fourier transform
-  nr <- nrow(M)
-  nc <- ncol(M)
-  areaM <- nr*ystep * nc*xstep #because theory uses rectangular windows, I'm going to assume a rectangular window to - maybe improve on this later
+  #pad if necessary #DFT approximation makes the assumption that function is 0 outside window anyway
+  if ((!is.null(suffspecres))
+    &&
+    ( any(suffspecres < 2*pi/(c(xstep*ncol(M),ystep*nrow(M)))) ))
+    {
+      #calculate padding required in each direction: kX such that suffspecresX > 2*pi/(xstep*nrow(M)*kX) (so output BETTER than suffspecres)
+      padfactor <- ceiling(2*pi/(c(xstep*ncol(M),ystep*nrow(M))*suffspecres))
+      Mpad <- matrix(0,nrow=nrow(M)*padfactor[2],ncol=ncol(M)*padfactor[1])
+      Mpad[1:nrow(M),1:ncol(M)] <- M
+      fM <- xstep*ystep*fft(Mpad) #xstep*ystep = scale to approximate Fourier transform
+  }
+  else {
+      fM <- xstep*ystep*fft(M) #xstep*ystep = scale to approximate Fourier transform
+  }
+  areaM <- xstep*ncol(M) * ystep*nrow(M)
   specdens <- (Re(fM)^2+Im(fM)^2)/(areaM) #divide by areaM to get spectral density (formula in Bohm)
   #currently specdens[i,j] corresponds to a spectral location of 
   #     y = 2pi*((i-1) mod numrow)/(length*ystep), x = 2pi*((j-1) mod numcol)/(length*xstep)
   # Rearrange this periodic function so that 
   # the origin of translations (0,0) is at matrix position (nr/2,nc/2) or close depending on whether nr is even or not
   # NB this could introduce an extra row and column
+  nr <- nrow(fM)
+  nc <- ncol(fM)
   if (nr %% 2 == 0){
     specdens <- specdens[ ((-nr/2):(nr/2)) %% (nr) + 1,]
     yrow <- ((-nr/2):(nr/2)) * 2*pi/(ystep*nr)
