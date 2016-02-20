f463ee94444fba53a157aa793e13ed1e03d5ed0f
Kassel Hingee <hinkass@gmail.com>
2016-02-20 10:59:18 +0800

    theoretical boolean function unit tests included

---
+++ stationaryracsinference/tests/testbooldeterministicdiscs.R
@@ -0,0 +1,16 @@
+## tests for Boolean simulations of deterministic disc code
+
+
+library(stationaryracsinference)
+
+## thcovarDeterministicDiscs check
+thcovarDeterministicDiscs(xrange=c(-10,10),yrange=c(-3,3),eps=c(1,1),0.0022064,10)
+
+## thspecdensAtOrigin check
+thspecdens_origin <- thspecdensAtOrigin(0.0022064,10)
+thspecdens_origin
+
+## quasithspecdens - check at origin
+specdens <- quasithspecdens(0.0022064,10)
+specdens[round(dim(specdens)[2]/2),round(dim(specdens)[1]/2)]
+
