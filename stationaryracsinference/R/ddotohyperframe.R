e17e9aee45ff3cab1b24aad3813c566345ffddda
Hingee <hin117@csiro.au>
2016-03-09 10:03:47 +0800

    some polishing of ddotohyperframe - took 45min cos I was trying to understand tessera

--- stationaryracsinference/R/ddotohyperframe.R
+++ stationaryracsinference/R/ddotohyperframe.R
@@ -4,8 +4,12 @@
 #' @description Extracts data from tessera's ddo objects and puts them into a hyperframe.
 #' 
 #' @param inddo Input ddo object
-#' @param slots A list of names of slots to extract into a hyperframe. If not supplied then function will try to extract all slots a
+#' @param slots A list of names of slots to extract into a hyperframe - each slot becomes a column in the hyperframe. 
+#' If not supplied then function will try to extract all slots using the first key-value pair as an example.
 #' @return A hyperframe, each column corresponding to a slot.
+#' 
+###########################################################
+#' 
 #' @examples
 #' applyestimators <- function(obs,w){
   #' #coverage fraction
@@ -36,7 +40,6 @@
 #' }
 #' 
 #' estimates <- datadr::kvPairs(
-#'      datadr::kvPair("fine",applyestimators(heather$fine,Frame(heather$fine))),
 #'      datadr::kvPair("medium",applyestimators(heather$medium,Frame(heather$medium))),
 #'      datadr::kvPair("coarse",applyestimators(heather$coarse,Frame(heather$coarse)))
 #'      )
@@ -44,10 +47,8 @@
 #' 
 #' hframe <- ddotohyperframe(estimatesddo,slots = c("coveragefrac","sphcontactxi"))
 #' hframe <- ddotohyperframe(estimatesddo)
-#' plot(hframe[,"sphcontactxi",drop=TRUE])
+#' plot(hframe[,"sphcontactxi",drop=TRUE], main = "Spherical Contact Distribution:\n Diggle's Heather Data")
 #' 
-
-
 ddotohyperframe <- function(inddo,slots="all"){
   if (length(slots)==1 && slots == "all"){
     warning("`all' option uses names of the first kv pair, if the values of each pair are different
@@ -56,11 +57,19 @@ ddotohyperframe <- function(inddo,slots="all"){
   } 
   hframe <- hyperframe()
   for (slotn in slots){
-    kvpairslist <- datadr::drLapply(inddo, function(v) v[[slotn]], combine=datadr::combCollect, params=list(slotn=slotn))  #returns a list of kvpairs
+    kvpairslist <- datadr::drLapply(inddo, function(v) v[[slotn]],
+                                    combine=datadr::combCollect,
+                                    params=c(slotn = slotn))  #returns a list of kvpairs
+    #note that the params field doesn't seem to be working
+    # - changing the parameter name to slotm makes the function error
+    
     valueslist <- lapply(kvpairslist,"[[","value")
+    names(valueslist) <- lapply(kvpairslist,"[[","key")
+    
     #don't do anything if all of length one then unlist! Objects like windows are all one length to. The trick will be to save them in the ddo as non-nested objects
     hframe <- cbind(hframe,valueslist)
   }
+  row.names(hframe) <- names(valueslist)
   names(hframe) <- slots
   return(hframe)
 }
