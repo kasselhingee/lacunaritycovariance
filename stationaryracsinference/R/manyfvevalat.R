fcb80583fa13b568aaec48b4461850c3b795e967
Kassel Hingee <hinkass@gmail.com>
2016-04-29 11:00:11 +0800

    manyfvevalat wont work for multiple value, so a single value parameter is as good as it gets, this is working fine

--- stationaryracsinference/R/manyfvevalat.R
+++ stationaryracsinference/R/manyfvevalat.R
@@ -3,6 +3,10 @@
 
 #' @description Given a list of fv objects evaluates each at the given argument, and returns a list of the same length.
 #' @param fvlist A list of fv objects
+#' @param argval The value of argument that each function is evaluated at.
+#' @param value  The function value to use, ".y" uses the default provided by the fv objects.
+#'  Like \texttt{value} in \texttt{as.function.fv()} but in this case can only only be a single string.
+#' @param extrapolate From \texttt{as.function.fv()}, whether to extrapolate the fv objects when the provided argval is outside their range.
 #' @return A list of values of each fv object at the given argument.
 #' 
 
@@ -11,14 +15,16 @@
 #' fv2 <- Hest(complement.owin(heather$coarse))
 #' fvlist <- list(fv1,fv2) 
 
-#' fvfunc01 <- as.function.fv(fv1)
+#' fvfunc01 <- as.function.fv(fv1,value=c("km","rs","han"))
 #' fvfunc01(0.19)
 #' 
 #' manyfvevalat(fvlist,0.19)
+#' 
+#' manyfvevalat(fvlist,0.19,value=c("rs","han"))
 
 manyfvevalat <- function(fvlist, argval, value=".y", extrapolate=TRUE){
+  stopifnot(length(value)==1)
   fvfuncs <- lapply(fvlist,as.function.fv,value=value,extrapolate=extrapolate)
   fvvals <- mapply(do.call,fvfuncs,args=list(r=list(argval)),SIMPLIFY=FALSE)
-  
   return(fvvals)
 }
