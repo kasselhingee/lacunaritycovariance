2342b9a97f2d454ef298bcc8c1be639fe8fed922
kassel on laptop <hinkass@gmail.com>
2016-07-01 12:09:05 +0200

    coverage probability help cleaned up

--- R/coverageprobabilityEst.R
+++ R/coverageprobabilityEst.R
@@ -1,12 +1,9 @@
 #' @title Estimate the coverage fraction of a stationary RACS
 #' 
 #' @description Estimates the probability of an aribitray point being inside a stationary RACS.
-#' Because the RACS is stationary this is equivalent to estimating \eqn{P(o\in\Xi)}. 
-#' The estimate is simply the fraction of \eqn{\Xi} in the observed window.
+#' The estimate is simply the fraction of \code{xi} in the observed window.
 #' 
 #' 
-#' @details in reality the area of xi is probably slightly different - depends on the sensing method of xi. Can assume really close though.
-
 #' @param xi An observation of a RACS.
 #' @param w The window of observation (not necessarily rectangular)
 #' @return an estimate of the coverage probability
