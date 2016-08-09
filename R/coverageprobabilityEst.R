70e1396ca983802d5a088ae319eafa1ef6f0cad2
U-NEXUS\hin117 <hin117@BANNISTER-FL.nexus.csiro.au>
2016-08-09 14:18:22 +0800

    merged in changes to DESCRIPTION and coverageprobabilityEst.R

--- R/coverageprobabilityEst.R
+++ R/coverageprobabilityEst.R
@@ -1,36 +1,29 @@
 #' @title Estimate the coverage fraction of a stationary RACS
 #' 
 #' @description Estimates the probability of an aribitray point being inside a stationary RACS.
-#' Because the RACS is stationary this is equivalent to estimating \eqn{P(o\in\Xi)}. 
-#' The estimate is simply the fraction of \eqn{\Xi} in the observed window.
+#' The estimate is simply the fraction of \code{xi} in the observed window.
 #' 
 #' 
-#' @details in reality the area of Xi is probably slightly different - depends on the sensing method of Xi. Can assume really close though.
-
-#' @param Xi An observation of a RACS.
+#' @param xi An observation of a RACS.
 #' @param w The window of observation (not necessarily rectangular)
 #' @return an estimate of the coverage probability
 #' @author Kassel Hingee 
 #' @import spatstat
 #' @export coveragefrac
 #' @examples
-#' XiOWIN <- heather$coarse
-#' windowOWIN <- Frame(heather$coarse)
-#' coverageProb <- coveragefrac(XiOWIN,windowOWIN)
-#' #
-#' #vegetation map of Balcatta park
-#' data(balcattapark_coarse)
-#' coverageProb <- coveragefrac(balcattapark_coarse$vegmask,balcattapark_coarse$boundary)
+#' xi <- heather$coarse
+#' obswindow <- Frame(heather$coarse)
+#' coverageProb <- coveragefrac(xi,obswindow)
 
 #' @keywords spatial nonparametric
-coveragefrac <- function(Xi,w){
-  stopifnot(is.owin(Xi))
+coveragefrac <- function(xi,w){
+  stopifnot(is.owin(xi))
   stopifnot(is.owin(w))   
-  XiInsideW <- intersect.owin(Xi,w)
-  areaXiInside <- area.owin(XiInsideW)
+  xiInsideW <- intersect.owin(xi,w)
+  areaxiInside <- area.owin(xiInsideW)
   areaWindow <- area.owin(w)
   
-  covProbEstimate <- areaXiInside/areaWindow
+  covProbEstimate <- areaxiInside/areaWindow
   return(covProbEstimate)
 }
 
