fe41d1b65085c1605e807eb6a659c8481ff0c61c
Kassel Hingee <hinkass@gmail.com>
2016-10-18 10:59:02 +0800

    covariance from merged covEst.R split into own file

--- R/covEst.R
+++ R/covEst.R
@@ -1,5 +1,5 @@
-#' @title Covariance estimates, also known as `two-point probabilities' for stationary RACS
-#' @export covariance covarianceEstAtPoint covarianceMapEst_direct
+#' @title Old covariance estimates
+#' @export covarianceEstAtPoint covarianceMapEst_direct
 #' @description 
 #' These functions estimate the covariance of a stationary random closed set. 
 #' The covariance is also known as the two-point coverage probability, and very closely related to the semivariogram.
@@ -12,13 +12,11 @@
 #' @param w The observation window in \code{owin} format. If itsn't included its taken to be the smallest rectangle enclosing \code{Xi}.
 #' @param w The boundary of the observation in OWIN format. This is needed for the cases where the observation is not a rectangular region.
 #' @param setCovBoundaryThresh to avoid instabilities of dividing by very small areas, any vector \eqn{v} set covariance of the boundary smaller than this threshold is given a covariance of NA 
-#' @param inclraw If TRUE the output will be two \code{im} objects one for the standard estimator and one raw estimate.
 
 #' @return 
 #' \item{comp1 }{An estimate (assuming stationarity of \eqn{\Xi}) that two points separated by \eqn{v} will be in \eqn{\Xi}.}
 #' \item{comp2 }{Denominator - The set covariance of the boundary \code{w}}
 #' \item{comp3 }{Numerator - The set covariance of \eqn{\Xi_{obs}}}
-#' @return \code{SpatStat} \code{im} objects containing the estimated covariances.
 #' 
 #' For \code{covarianceEstAtPoint} these are single numerical values; for \code{covarianceMapEst_direct} they are matrices; for \code{covariance} they are objects of SpatStat's \code{im} class.
 
@@ -26,12 +24,6 @@
 #' @examples
 #' XiOWIN <- heather$coarse
 #' windowOWIN <- Frame(heather$coarse)
-#' 
-#' data(balcattapark_coarse)
-#' xi <- balcattapark_coarse$vegmask
-#' w <- balcattapark_coarse$boundary
-#' covar <- covariance(xi,w,inclraw=FALSE)
-#' plot(covar,clipwin=owin(xrange=c(-10,10),yrange=c(-10,10)),axes=TRUE)
 #' 
 #' coverageProb <- coveragefrac(XiOWIN,windowOWIN)
 #' 
@@ -57,47 +49,14 @@
 
 #' @details 
 #' The reduced sample estimator is [1]
-#' \eqn{ \hat{C}(v) = \frac{|\Xi \cap W \cap ((\Xi \cap W ) \oplus v)|}{|W \cap W\oplus v|}}
-#' and the raw estimate (if requested) is
-#' \eqn{\hat{\tilde{C}}(v) = \frac{|\Xi \cap W \cap ((\Xi \cap W ) \oplus v)|}{|W|}.}
+#' \eqn{ \hat{C}(v) = \frac{|\Xi \cap W \cap ((\Xi \cap W ) \oplus v)|}{|W \cap W\oplus v|}.}
 #' \code{covariance} uses Fourier transforms to calculate set covariances (using \code{\link[spatstat]{setcov}} function). It is much faster (500 times faster in one comparison) than \code{covarianceMapEst_direct}.
 #' Vectors with small set covariance of the window are eliminated (using \code{setCovBoundaryThresh} because they cause the covariance to be enormous)
 
-#' @references [1] Molchanov, I. (1997) Statistics of the Boolean Model for Practitioners and Mathematicians. John Wiley & Sons.
 
-covariance <- function(Xi,w=NULL,inclraw=FALSE,setCovBoundaryThresh = 0.1*area.owin(w)){
-   if (is.owin(xi)){
-      if (!is.null(w)){xi <- intersect.owin(xi,w)}
-      else {w <- Frame(xi)}
-      setcovXi <- setcov(xi)
-      setcovB <- setcov(w)
-   }
-   else if (is.im(xi)){
-      if (!is.null(w)) {
-          winim <- as.im(w,xy=xi)
-          xi <- eval.im(xi*winim)
-      }
-      else {w <- Frame(xi)}
-      setcovXi <- imcov(xi)
-      setcovB <- setcov(w)
-   }
-   else {
-      print("ERROR: Input xi is not an image or owin object")
-      return(NULL)
-   }
-   setcovB[setcovB<setCovBoundaryThresh] <- NA #make NA any values that are too small and lead to division to close to 0
-   covar <- eval.im(setcovXi/setcovB,harmonize=TRUE)
-   if (!inclraw) {return(covar)}
-   if (inclraw) {
-      return(list(rs = covar,
-                  raw = setcovXi/area(w)))
-   }
-}
-
-#catch the warning message about harmonising 
 
 ########################################
-#' @rdname covariance
+#' @rdname covarianceEstAtPoint
 #' @details \code{covarianceEstAtPoint} estimates the covariance of a single vector \eqn{v} by ratioing the set covariance of \code{Xi} to the set covariance of of observation window \code{w}. Set covariance is calculated by intersecting a set with a translated copy of itself.
 #' @param v A 2D vector in c(x,y) format.
 
@@ -118,7 +77,7 @@ covarianceEstAtPoint <- function(Xi,w,v){
 
 
 #######################################
-#' @rdname covariance
+#' @rdname covarianceEstAtPoint
 #' @details 
 #' \code{covarianceMapEst_direct} estimates covariance on a regular grid using the resolution of \code{Xi}. The regular grid extends to \code{maxXshiftdistance} and \code{maxYshiftdistance} in the x and y components respectively. It uses \code{covarianceEstAtPoint} to estimate the covariance at each grid point.
 #' Ignores point estimates that use an area smaller than 10% of the window.
--- R/covariance.R
+++ R/covariance.R
@@ -1,27 +1,45 @@
-#' @title Covariance (Two-Point Probability) of a Stationary RACS
+#' @title Covariance estimates, also known as `two-point probabilities' for stationary RACS
 #' @export covariance
-#' @description Estimates covariance using the reduced sample method suggested by Molchanov ... and a raw uncorrected estimate if requested.
+#' @description 
+#' These functions estimate the covariance of a stationary random closed set. 
+#' The covariance is also known as the two-point coverage probability, and very closely related to the semivariogram.
+#'  The covariance of a vector \eqn{v} is the probability of two points separated by a \eqn{v} being covered by \eqn{\Xi}.
+#' @author{Kassel Hingee}
 
-#' @details The reduced sample estimator is [1]
-#' \eqn{ \hat{C}(v) = \frac{|\Xi \cap W \cap ((\Xi \cap W ) \oplus v)|}{|W \cap W\oplus v|}}
-#' and the raw estimate is
-#' \eqn{\hat{\tilde{C}}(v) = \frac{|\Xi \cap W \cap ((\Xi \cap W ) \oplus v)|}{|W|}.}
-#' @references [1] Molchanov, I. (1997) Statistics of the Boolean Model for Practitioners and Mathematicians. John Wiley & Sons.
 
-#' @param Xi An observed RACS in an \code{owin} object or black and white image (\code{im} format).
+
+#' @param Xi An observation (in spatstat owin format or black and white \code{im} object) of the RACS of interest.
 #' @param w The observation window in \code{owin} format. If itsn't included its taken to be the smallest rectangle enclosing \code{Xi}.
+#' @param setCovBoundaryThresh to avoid instabilities of dividing by very small areas, any vector \eqn{v} set covariance of the boundary smaller than this threshold is given a covariance of NA 
 #' @param inclraw If TRUE the output will be two \code{im} objects one for the standard estimator and one raw estimate.
 
 #' @return \code{SpatStat} \code{im} objects containing the estimated covariances.
 
+
 #' @examples
+#' XiOWIN <- heather$coarse
+#' windowOWIN <- Frame(heather$coarse)
+
 #' data(balcattapark_coarse)
 #' xi <- balcattapark_coarse$vegmask
 #' w <- balcattapark_coarse$boundary
 #' covar <- covariance(xi,w,inclraw=FALSE)
 #' plot(covar,clipwin=owin(xrange=c(-10,10),yrange=c(-10,10)),axes=TRUE)
+#' 
 
-covariance <- function(xi,w=NULL,inclraw=FALSE){
+#' @keywords spatial nonparametric
+
+#' @details 
+#' The reduced sample estimator is [1]
+#' \eqn{ \hat{C}(v) = \frac{|\Xi \cap W \cap ((\Xi \cap W ) \oplus v)|}{|W \cap W\oplus v|}}
+#' and the raw estimate (if requested) is
+#' \eqn{\hat{\tilde{C}}(v) = \frac{|\Xi \cap W \cap ((\Xi \cap W ) \oplus v)|}{|W|}.}
+#' \code{covariance} uses Fourier transforms to calculate set covariances (using \code{\link[spatstat]{setcov}} function). It is much faster (500 times faster in one comparison) than \code{covarianceMapEst_direct}.
+#' Vectors with small set covariance of the window are eliminated (using \code{setCovBoundaryThresh} because they cause the covariance to be enormous)
+
+#' @references [1] Molchanov, I. (1997) Statistics of the Boolean Model for Practitioners and Mathematicians. John Wiley & Sons.
+
+covariance <- function(Xi,w=NULL,inclraw=FALSE,setCovBoundaryThresh = 0.1*area.owin(w)){
    if (is.owin(xi)){
       if (!is.null(w)){xi <- intersect.owin(xi,w)}
       else {w <- Frame(xi)}
@@ -41,14 +59,13 @@ covariance <- function(xi,w=NULL,inclraw=FALSE){
       print("ERROR: Input xi is not an image or owin object")
       return(NULL)
    }
+   setcovB[setcovB<setCovBoundaryThresh] <- NA #make NA any values that are too small and lead to division to close to 0
    covar <- eval.im(setcovXi/setcovB,harmonize=TRUE)
    if (!inclraw) {return(covar)}
    if (inclraw) {
       return(list(rs = covar,
                   raw = setcovXi/area(w)))
    }
-      #imcov for xi an image
 }
 
 #catch the warning message about harmonising 
-#what to do about division by things close to 0?
