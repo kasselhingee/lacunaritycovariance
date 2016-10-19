e8aec4ce536fbbc671e9c9740dea63244f51ca4e
Kassel Hingee <hinkass@gmail.com>
2016-10-19 10:33:30 +0800

    lac and lacgb tests updated

--- tests/testlac.R
+++ tests/testlac.R
@@ -2,6 +2,6 @@ suppressPackageStartupMessages(library(stationaryracsinference))
 
 covar <- covariance(heather$coarse)
 p <- area(heather$coarse)/area(Frame(heather$coarse))
-bandwidths <- 2
-lac <- lac(bandwidths,covar,p)
-abs(lac-0.06496912) < 1E-6
+sidelengths <- 2.2
+lac <- lac(sidelengths,covar,p)
+lac
--- tests/testlacgb.R
+++ tests/testlacgb.R
@@ -1,7 +1,7 @@
 suppressPackageStartupMessages(library(stationaryracsinference))
 
-img <- as.im(heather$fine,eps=heather$fine$xstep)
-bandwidths <- 2
+img <- as.im(heather$coarse,eps=heather$coarse$xstep)
+bandwidths <- c(1.1)
 lac <- lacgb(img,bandwidths)
-abs(lac$RS-0.07396426) < 1E-6
-abs(lac$nobord-0.279406) < 1E-6
+lac$RS
+lac$nobord
