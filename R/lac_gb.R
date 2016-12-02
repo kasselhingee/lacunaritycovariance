87dc42862fa2a5b01b2f300d0b671277ce3c4f77
Kassel Hingee <hinkass@gmail.com>
2016-12-02 15:40:46 +0800

    fixed bug so that NA is returned by lacgb0 instead of NULL

--- R/lac_gb.R
+++ R/lac_gb.R
@@ -38,6 +38,7 @@ lacgb <- function(img,sidelengths,inclraw=TRUE,W=Frame(img)){
 
   lacs <- mapply(lacgb0,bX=rpix,bY=rpix,MoreArgs=list(img=img, W=obsvd),SIMPLIFY=FALSE,inclraw)
 
+
 if (inclraw){
   nobord <- unlist(lapply(lacs, `[[`, 1) )
   RS <- unlist(lapply(lacs, `[[`, 2) )
@@ -85,7 +86,7 @@ lacgb0 <- function(img,bX,bY,inclraw,W=Frame(img)){
     lacA <- ss2A/(smA^2) -1
   }
   allowedBoxCentres <- erosionAny(W,Frame(kernelfcn))
-  if (is.empty(allowedBoxCentres)){lacRS=NULL}
+  if (is.empty(allowedBoxCentres)){lacRS=NA}
   else {
     areafracsRS <-  as.im(areafracs,W=Frame(img)) 
     rsW <- as.im(allowedBoxCentres,xy=areafracsRS) #note erosion by distance b is not quite the same as erosion by a square of "radius" b - but no function is available for the correct method yet
