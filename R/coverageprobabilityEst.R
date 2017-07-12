e3efac8b4eecefb618d948fa5e01e05b0cf177dc
Kassel Hingee <hinkass@gmail.com>
2017-07-12 11:08:25 +0800

    coverage fraction manual entry cleaned up. I've noticed a notational discrepancy between using X or xi for the realisation of a RACS

--- R/coverageprobabilityEst.R
+++ R/coverageprobabilityEst.R
@@ -1,12 +1,17 @@
 #' @title Estimate the coverage fraction of a stationary RACS
 #' 
-#' @description Estimates the probability of an aribitray point being inside a stationary RACS.
-#' The estimate is simply the fraction of \code{xi} in the observed window.
+#' @description 
+#' The coverage fraction of a stationary RACS is also known as the coverage probability and is the probability that an arbitrary point is covered by the RACS.
+#' Given a realisation, \eqn{X}, in window \eqn{W} of a stationary RACS the coverage fraction estimate that this function calculates is the fraction of \eqn{W} covered by \eqn{X}.
+#' This estimate relies on the window \eqn{W} being large compared to the spatial dependence of the RACS.
+#' See [1, section 6.4.2] for more theoretical details.
 #' 
+#' @references [1] Chiu, S.N., Stoyan, D., Kendall, W.S. and Mecke, J. (2013) Stochastic Geometry and Its Applications, 3rd ed. Chichester, United Kingdom: John Wiley & Sons.
+ 
 #' 
-#' @param xi An observation of a RACS.
-#' @param w The window of observation (not necessarily rectangular)
-#' @return an estimate of the coverage probability
+#' @param xi An observation of a RACS in \pkg{spatstat}'s \code{owin} format.
+#' @param w The window of observation (not necessarily rectangular) also in \code{owin} format.
+#' @return An estimate of the coverage probability
 #' @author Kassel Hingee 
 #' @import spatstat
 #' @export coveragefrac
