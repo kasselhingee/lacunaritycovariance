ce4fe25dd6397e4e8e96feb583b27cdddd14a682
Kassel Hingee <hinkass@gmail.com>
2017-08-28 16:14:13 +0800

    mvlgb updated to return a column that is simply MVL

--- tests/testlacgb.R
+++ tests/testlacgb.R
@@ -3,5 +3,5 @@ suppressPackageStartupMessages(library(racssummfuncs))
 img <- as.im(heather$coarse,eps=heather$coarse$xstep, na.replace=0)
 sidel <- c(2.2)
 lac <- lacgb(img,sidel)
-lac$RS
+lac$MVL
 lac$raw
