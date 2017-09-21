42c8c41784ff1c13d1fa11f7bc9182eb20e50204
kassel on laptop <hinkass@gmail.com>
2017-09-21 10:39:01 +1000

    pdf manual couldn't make the Xi_H object names in scdcontagion

--- R/contagSphCont.R
+++ R/contagSphCont.R
@@ -15,27 +15,27 @@
 #' The main difference to classical contagion is that disc-state contagion is based on the spherical contact distribution instead of pixel neighbours.
 #' One impact of this design is that the distance with which to quantify the mixing between \eqn{\Xi} and the background may be chosen by the user by choosing the disc radius (for classical contagion this distance is fixed by the image resolution).
 #' 
-#' @param Xi_H Conditional spherical contact distribution function for \eqn{\Xi}. 
+#' @param XiH Conditional spherical contact distribution function for \eqn{\Xi}. 
 #' Typically this is an \code{fv} object but could also be a vector of values.
-#' In applications \code{Xi_H} would likely be estimated from an image using \code{\link{Hest}} in \pkg{spatstat}.
-#' @param Xic_H Conditional spherical contact distribution for the complement of \eqn{\Xi}. 
+#' In applications \code{XiH} would likely be estimated from an image using \code{\link{Hest}} in \pkg{spatstat}.
+#' @param XicH Conditional spherical contact distribution for the complement of \eqn{\Xi}. 
 #' This is called the Conditional Core Probability in Hingee 2016.
 #' Typically this is an \code{fv} object but could also be a vector of values.
-#' In applications \code{Xi_H} would likely be estimated from an image using \code{\link{Hest}} in \pkg{spatstat}.
+#' In applications \code{XiH} would likely be estimated from an image using \code{\link{Hest}} in \pkg{spatstat}.
 #' @param p  The coverage probability of \eqn{\Xi}.
 #' In applications to images an estimate of the coverage probability can be obtained using \code{\link{coverageprob}}.
 #' @param normalise Optional. If TRUE \code{scdcontagion} normalises the results so that all RACS return a value between 0 and 1. Default is FALSE. 
-#' @details Xi_H should be a function of radius that gives (or estimates) the probability of a disc of radius \eqn{r} not intersecting \eqn{\Xi} if the disc's centre is not in \eqn{\Xi} 
-#' \deqn{\code{Xi_H}(r) = P(B_r(x) \subseteq \Xi^c | x \in \Xi^c).}
-#' Similarly \code{Xic_H} should be an estimate of the probability of a disc being fully contained in \eqn{\Xi} given its centre is in \eqn{\Xi}
-#' \deqn{\code{Xic_H}(r)\approx P(B_r(x) \subseteq \Xi | x \in \Xi).}
+#' @details XiH should be a function of radius that gives (or estimates) the probability of a disc of radius \eqn{r} not intersecting \eqn{\Xi} if the disc's centre is not in \eqn{\Xi} 
+#' \deqn{\code{XiH}(r) = P(B_r(x) \subseteq \Xi^c | x \in \Xi^c).}
+#' Similarly \code{XicH} should be an estimate of the probability of a disc being fully contained in \eqn{\Xi} given its centre is in \eqn{\Xi}
+#' \deqn{\code{XicH}(r)\approx P(B_r(x) \subseteq \Xi | x \in \Xi).}
 #' These can both be obtained using \code{\link{Hest}} in \pkg{spatstat}.
-#' For \code{Xic_H} take care to apply Hest to the complement of \eqn{\Xi} with the observation window \eqn{W}.
+#' For \code{XicH} take care to apply Hest to the complement of \eqn{\Xi} with the observation window \eqn{W}.
 #' 
 #' If \code{normalise} is \code{TRUE} then the result is divided by 
 #' \eqn{\frac{-4}{e}ln(\frac{1}{e})} and added to 1 so that the return value is between 0 and 1 for all possible \eqn{\Xi}.
 #'
-#' @return An \code{fv} object or a vector the same length as \code{Xi_H} corresponding to the contagion at each r value of \code{Xi_H}
+#' @return An \code{fv} object or a vector the same length as \code{XiH} corresponding to the contagion at each r value of \code{XiH}
 
 #' @references 
 #' Hingee, K.L. (2016) Statistics for Patch Observations. ISPRS - International Archives of the Photogrammetry, Remote Sensing and Spatial Information Sciences pp. 235-242. ISPRS.
@@ -47,45 +47,45 @@
 #' p <- coverageprob(xi,Frame(xi))
 #' xi_H <- Hest(xi,W=obswindow) #Sph. Contact Distrution Estimate
 #' xic_H <- Hest(complement.owin(xi),W=obswindow) #Conditional Core Prob. Estimate
-#' plot(xi.H,type="l",col="red") 
-#' lines(xic.H,type="l",col="black") 
+#' plot(xi_H,type="l",col="red") 
+#' lines(xic_H,type="l",col="black") 
 #' 
 #' contagion <- scdcontagion(xi_H,xic_H,p)
 #' plot(contagion)
 #' 
 #' @keywords spatial nonparametric 
-scdcontagion <- function(Xi_H, Xic_H, p, normalise=FALSE){
+scdcontagion <- function(XiH, XicH, p, normalise=FALSE){
   returnfv <- FALSE
   unitnames <- NULL
-  if (is.fv(Xi_H) && is.fv(Xic_H)) {
+  if (is.fv(XiH) && is.fv(XicH)) {
     returnfv <- TRUE
-    unitnames <- unitname(Xi_H)
-    fvin <- list(Xi_H = Xi_H, Xic_H = Xic_H)
-    Xi_Hf <- as.function.fv(Xi_H, value = ".y", extrapolate = TRUE)
-    Xic_Hf <- as.function.fv(Xic_H, value = ".y", extrapolate = TRUE)
-    argranges <- lapply(list(Xi_H = Xi_H, Xic_H = Xic_H), argumentrange)
+    unitnames <- unitname(XiH)
+    fvin <- list(XiH = XiH, XicH = XicH)
+    XiHf <- as.function.fv(XiH, value = ".y", extrapolate = TRUE)
+    XicHf <- as.function.fv(XicH, value = ".y", extrapolate = TRUE)
+    argranges <- lapply(list(XiH = XiH, XicH = XicH), argumentrange)
     ## determine finest resolution (from spatstat)
     xsteps <- sapply(fvin, argumentstep)
     finest <- which.min(xsteps)
     ## extract argument values (from spatstat)
     xvals <- with(fvin[[finest]], .x)
-    fvwlargestarg <- which.max(list(Xi_H = argranges$Xi_H[[2]],
-                                   Xic_H = argranges$Xic_H[[2]]))
+    fvwlargestarg <- which.max(list(XiH = argranges$XiH[[2]],
+                                   XicH = argranges$XicH[[2]]))
     xvals <- c(xvals,
       with(fvin[[fvwlargestarg]], .x)[
         with(fvin[[fvwlargestarg]], .x) > max(xvals)
       ])
     ##
 
-    Xi_H <- Xi_Hf(xvals)
-    Xic_H <- Xic_Hf(xvals)
+    XiH <- XiHf(xvals)
+    XicH <- XicHf(xvals)
   }
-  probofstate <- matrix(NA, nrow = 4, ncol = length(Xi_H))
+  probofstate <- matrix(NA, nrow = 4, ncol = length(XiH))
   rownames(probofstate) <- c("P11", "P10", "P01", "P00")
-  probofstate["P11", ] <- p * (1 - Xic_H)
-  probofstate["P10", ] <- p * Xic_H
-  probofstate["P00", ] <- (1 - p) * (1 - Xi_H)
-  probofstate["P01", ] <- (1 - p) * (Xi_H)
+  probofstate["P11", ] <- p * (1 - XicH)
+  probofstate["P10", ] <- p * XicH
+  probofstate["P00", ] <- (1 - p) * (1 - XiH)
+  probofstate["P01", ] <- (1 - p) * (XiH)
 
   tempstates <- probofstate
   tempstates[probofstate < 1E-8] <- 1
