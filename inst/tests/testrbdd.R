b8ddd53e0bee3cbc7cc925b7acfe8ba9cc8b9908
U-NEXUS\hin117 <hin117@DANCE-WF.nexus.csiro.au>
2017-09-22 16:29:54 +0800

    moved rbdd tests to testthat format - note that the 2nd order properties of rbdd are tested by covariance already

---
+++ inst/tests/testrbdd.R
@@ -0,0 +1,41 @@
+context("RACS simulation")
+
+test_that("rbdd produces simulations with the correct area fraction", {
+  #Boolean model with discs of radius 10.
+  #The intensity has been chosen such that the true coverage fraction is very close to 0.5.
+  discr <- 10
+  w <- owin(xrange=c(0,100),c(0,100))
+  lambda <- 2.2064E-3
+  xi <- rbdd(lambda, discr, w, seed=6549)
+  expect_equal(coveragefrac(xi, w), 0.4644903, tolerance = 1E-7)
+  
+  #theoretical coveragefrac
+  truecoveragefrac <- bdd.coverageprob(lambda,discr)
+  
+  ## use covariance to form approximate confidence interval
+  thcovariance <- bdd.covar(xrange=c(-100,100),yrange=c(-100,100),eps=c(1,1),lambda, discr)
+  setcovB <- setcov(w)
+  tryCatch(integrand <- eval.im((thcovariance-truecoveragefrac^2)*setcovB,harmonize = TRUE),
+           warning = function(w) {
+             if (!grepl("simpleWarning:  the images .thcovariance. and .setcovB. were not compatible\n", paste0(w))){
+               warning(w)
+             }
+           })
+  exactvariance <- (1/(area.owin(w))^2)*sum(integrand)*integrand$xstep*integrand$ystep
+  ##assume that window large enough that distribution of estimator is Gaussian
+  ##So half-width of 0.05 conf interval is
+  confint_halfwidth <- -qnorm(0.025, mean = 0, sd=sqrt(exactvariance))
+  
+  #test on random simulations (seed unfixed)
+  xi <- rbdd(lambda, discr, w)
+  expect_equal(coveragefrac(xi, w), truecoveragefrac, tolerance = confint_halfwidth)
+})
+
+test_that("Theoretical spectral density calculation is historically consistent", {
+  discr <- 10
+  lambda <- 2.2064E-3
+  thspecdens_origin <- bdd.specdensAtOrigin(lambda, discr)
+  expect_equal(thspecdens_origin, 64.41312)
+  #I think the above number was checked to be consistent with plot in the Bohm paper
+})
+  
