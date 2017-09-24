229066ba0378e5ee73d0964fb308830969c9d2e5
kassel on laptop <hinkass@gmail.com>
2017-09-24 09:10:55 +0800

    testing of rbdd: made tolerance of thspecdens more sensible, removed tryCatch in confidence interval calculation

--- tests/testthat/test-rbdd.R
+++ tests/testthat/test-rbdd.R
@@ -14,13 +14,9 @@ test_that("rbdd produces simulations with the correct area fraction", {
   
   ## use covariance to form approximate confidence interval
   thcovariance <- bdd.covar(xrange=c(-100,100),yrange=c(-100,100),eps=c(1,1),lambda, discr)
-  setcovB <- setcov(w)
-  tryCatch(integrand <- eval.im((thcovariance-truecoveragefrac^2)*setcovB,harmonize = TRUE),
-           warning = function(w) {
-             if (!grepl("simpleWarning:  the images .thcovariance. and .setcovB. were not compatible\n", paste0(w))){
-               warning(w)
-             }
-           })
+  setcovB <- setcov(w, eps = c(thcovariance$xstep, thcovariance$ystep))
+  harmims <- harmonise.im(thcovar = thcovariance, setcovwin = setcovB)
+  integrand <- eval.im((thcovariance-truecoveragefrac^2)*setcovB, envir = list(thcovariance = harmims[[thcovar]], setcovB = harmims[[setcovwin]]))
   exactvariance <- (1/(area.owin(w))^2)*sum(integrand)*integrand$xstep*integrand$ystep
   ##assume that window large enough that distribution of estimator is Gaussian
   ##So half-width of 0.05 conf interval is
@@ -35,7 +31,7 @@ test_that("Theoretical spectral density calculation is historically consistent",
   discr <- 10
   lambda <- 2.2064E-3
   thspecdens_origin <- bdd.specdensAtOrigin(lambda, discr)
-  expect_equal(thspecdens_origin, 64.41312)
+  expect_equal(thspecdens_origin, 64.41312, tolerance = 1E-5)
   #I think the above number was checked to be consistent with plot in the Bohm paper
 })
   
