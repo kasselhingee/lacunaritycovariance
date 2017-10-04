2fd9cf9b86e12b745436ace91b4cdbc21a4e6795
Kassel Hingee <hinkass@gmail.com>
2017-10-04 15:40:37 +0800

    testing scdcontagion return format

---
+++ tests/testthat/test-scdcontagion.R
@@ -0,0 +1,19 @@
+test_that("scdcontagion is returning the correct format", {
+  xi <- heather$coarse
+  obswindow <- Frame(heather$coarse)
+  p <- coverageprob(xi, Frame(xi))
+  xiH <- Hest(xi, W = obswindow) #Sph. Contact Distrution Estimate
+  xicH <- Hest(complement.owin(xi), W = obswindow) #Conditional Core Prob. Estimate
+  contagion <- scdcontagion(xiH, xicH, p, normalise = TRUE)
+
+  expect_is(contagion, "fv")
+  expect_equal(names(contagion), as.character(list("r","contag")))
+
+  coldescriptions <- attr(contagion, "desc")
+  expect_equal(coldescriptions, as.character(list("radius", "normalised SCD contagion estimate")))
+
+  contagion <- scdcontagion(xiH, xicH, p, normalise = FALSE)
+  coldescriptions <- attr(contagion, "desc")
+  expect_equal(coldescriptions, as.character(list("radius", "unnormalised SCD contagion estimate")))
+})
+
