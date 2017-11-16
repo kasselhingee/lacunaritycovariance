1c773d4b86e101dd04b75e0b2a98dc406ee76ef5
U-NEXUS\hin117 <hin117@DANCE-WF.nexus.csiro.au>
2017-11-16 12:05:59 +0800

    made sure image is only the size of obswin then complement.owin doesn't have to be a careful

--- R/varofobsarea.R
+++ R/varofobsarea.R
@@ -6,9 +6,9 @@
 
 
 #' @param xi An observation of the RACS of interest in 1, 0, or NA valued pixels.
-#' Pixels must be square.
+#' Pixels must be square. Must be a spatstat im object.
 #' @param obswin Observation window
-#' @param corrrad Radius of the step function in the correlation
+#' @param corrrad Radius of the step function in the correlation (in the same units as xi)
 #' @param corrstepheight Height of the step in the correlation
 #' @param p01 Probability of an randomly chosen fallibly-classified
 #'  tree pixel is not in the true tree canopy
@@ -18,7 +18,7 @@
 #' @examples
 #' xi <- as.im(heather$coarse, na.replace = 0)
 #' obswin <- Frame(xi)
-#' corrrad <- 10 #10 pixels
+#' corrrad <- 5 #in units of image
 #' corrstepheight <- 0.9
 #' p01 <- 0.05
 #' p10 <- 0.01
@@ -32,9 +32,10 @@
 #' To install OpenImageR had to install libtiff5-dev on my ubuntu machine
  
 expectedarea <- function(xi, obswin, p01=NA, p10=NA){
-  xi[complement.owin(obswin, frame = Frame(xi))] <- 0
+  xi <- xi[as.rectangle(obswin), drop=FALSE]
+  xi[complement.owin(obswin)] <- 0
   xic <- 1-xi
-  xic[complement.owin(obswin, frame = Frame(xi))] <- 0  
+  xic[complement.owin(obswin)] <- 0  
   
   return((sum(xi) * (1 - p01) + sum(xic) * p10) * xi$xstep * xi$ystep)
 }
@@ -57,7 +58,7 @@ varofobsarea.v1 <- function(xi, obswin, corrrad, corrstepheight, p01, p10){
 
 convandintersectsum <- function(xi, corrrad){
   # create disc kernel
-  kernel.owin <- disc(radius = corrrad, mask = TRUE, eps = 1)#make everything in pixel units
+  kernel.owin <- disc(radius = corrrad, mask = TRUE, eps = c(xi$xstep, xi$ystep)) #make disc at same resolution as xi
   kernel.m <- as.matrix(kernel.owin)
 
   xi.m <- as.matrix(xi)
