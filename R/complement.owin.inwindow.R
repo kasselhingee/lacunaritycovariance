4b6ba1f58cc4677525500af0d0768a68b40ccaef
Kassel Hingee <hinkass@gmail.com>
2018-02-15 16:11:17 +0800

    added a function for complementing robustly - it could be more robust

--- R/complement.owin.inwindow.R
+++ R/complement.owin.inwindow.R
@@ -29,7 +29,7 @@
 # xipc <- complement.owin.inwindow(xip, w)
 # expect_equal(area.owin(union.owin(intersect.owin(xip, w), xipc)), area.owin(w), tolerance = area.owin(w)/1000)
 # expect_equal(area.owin(intersect.owin(xip, xipc)), 0, tolerance = area.owin(w)/1E6)
-xic <- complement.owin.inwindow(square(), square(r=0.5))
+# xic <- complement.owin.inwindow(square(), square(r=0.5))
 
 #' @details 
 #' The usual complement.owin function in spatstat does not (to my knowledge) accomplish the task that complement.owin.inwindow does as
