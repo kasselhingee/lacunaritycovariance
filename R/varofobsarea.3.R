709a291a6b66e98846142937ad56be1a69f52f78
U-NEXUS\hin117 <hin117@DANCE-WF.nexus.csiro.au>
2018-02-19 09:58:53 +0800

    corrected v3 to now take owin objects

--- R/varofobsarea.3.R
+++ R/varofobsarea.3.R
@@ -5,9 +5,8 @@
 
 
 
-#' @param xi An observation of the RACS of interest in 1, 0, or NA valued pixels.
-#' Pixels must be square. Must be a spatstat im object.
-#' @param obswin Observation window
+#' @param xi An observation of the RACS of interest in owin form.
+#' @param obswin Observation window in owin format
 #' @param corrrad Radius of the step function in the correlation (in the same units as xi)
 #' @param corrstepheight Height of the step in the correlation
 #' @param p21 Probability that a location fallibly classified into the cover of interest is really outside the cover of interest
@@ -15,7 +14,7 @@
 
 
 #' @examples
-#' xi <- as.im(heather$coarse, na.replace = 0)
+#' xi <- heather$coarse
 #' obswin <- Frame(xi)
 #' corrrad <- 5 #in units of image
 #' corrstepheight <- 0.9
@@ -31,27 +30,25 @@
 #' To install OpenImageR had to install libtiff5-dev on my ubuntu machine
  
 sae.v3.mean <- function(xi, obswin, p21=NA, p12=NA){
-  xi <- xi[as.rectangle(obswin), drop=TRUE]
-  xi[complement.owin(obswin, frame = Frame(dilation.owin(obswin,1)))] <- 0
-  xic <- 1-xi
-  xic[complement.owin(obswin, frame = Frame(dilation.owin(obswin,1)))] <- 0  
-  
-  return((sum(xi) * (1 - p21) + sum(xic) * p12) * xi$xstep * xi$ystep)
+  xi.area <- area.owin(xi)
+  window.area <- area.owin(obswin)
+  return(xi.area - ( xi.area * p21 ) + ( (window.area - xi.area) * p12))
 }
 
 #' @describeIn sae.v3.mean The variance of the expected area using Small Area Estimation Method - Version 3. 
 #' This methods assumes that the omission and comission errors are independent and different processes and that the correlation between errors (within each of theses processes) is a step function with radius \code{corrrad}.
 sae.v3.var <- function(xi, obswin, corrrad, corrstepheight, p21, p12){
-  xi <- xi[as.rectangle(obswin), drop=TRUE]
-  xi[complement.owin(obswin, frame = Frame(dilation.owin(obswin,1)))] <- 0
+  xi.im <- as.im(xi, na.replace = 0) #warning is 0 outside obswin too!
+  xi.im <- xi.im[as.rectangle(obswin), drop=TRUE]
   #radius filter of the cover type of interest
   xiconvsum <- convandintersectsum(xi, corrrad)
   varfromcomm <- p21 * (1 - p21) * ( (1 - corrstepheight) * sum(xi) + corrstepheight * xiconvsum)
   
   #radius filter of the alternate cover type
-  xic <- 1-xi
-  xic[complement.owin(obswin, frame = Frame(dilation.owin(obswin,1)))] <- 0  
-  xicconvsum <- convandintersectsum(xic, corrrad)
+  xi.c <- setminus.owin(obswin, xi) #I should do this instead of complement.owin.inwindow
+  xi.c.im <- as.im(xi.c, na.replace = 0)
+  xi.c.im <- xi.c.im[as.rectangle(obswin), drop=TRUE]
+  xicconvsum <- convandintersectsum(xi.c.im, corrrad)
   varfromomm <- p12 * (1 - p12) * ( (1 - corrstepheight) * sum(xic) + corrstepheight * xicconvsum)
   
   return((varfromomm + varfromcomm) * xi$xstep^2 * xi$ystep^2)
