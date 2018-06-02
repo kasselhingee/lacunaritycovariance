abaded9926e3615b73b5cdf9dacfbb8666ae5d33
Kassel Hingee <hinkass@gmail.com>
2018-06-02 18:13:45 +0800

    corrected old use of lac(

--- tests/testthat/test-mvl.R
+++ tests/testthat/test-mvl.R
@@ -5,13 +5,13 @@ test_that("mvlc() warns of unexpected inputs", {
   p <- area(heather$coarse) / area(Frame(heather$coarse))
   sidelengths <- 2.2
   img <- as.im(heather$coarse, eps = heather$coarse$xstep, na.replace = 0)
-  expect_error(lac(sidelengths, covariance = covar, p = p, xiim = img),
+  expect_error(mvlc(sidelengths, covariance = covar, p = p, xiim = img),
                  regexp = "Either covariance and p must be supplied or xiim supplied.")
 
-  expect_error(lac(sidelengths, covariance = covar, xiim = img),
+  expect_error(mvlc(sidelengths, covariance = covar, xiim = img),
                  regexp = "Either covariance and p must be supplied or xiim supplied.")
 
-  expect_error(lac(sidelengths, p = p, xiim = img),
+  expect_error(mvlc(sidelengths, p = p, xiim = img),
                  regexp = "Either covariance and p must be supplied or xiim supplied.")
 
   sidel <- c(2.2)
@@ -33,7 +33,7 @@ test_that("MVLc estimates are historically consistent", {
   covar <- racscovariance(heather$coarse)
   p <- area(heather$coarse) / area(Frame(heather$coarse))
   sidelengths <- 2.2
-  lac <- lac(sidelengths, covar, p)
+  lac <- mvlc(sidelengths, covar, p)
   expect_equal(lac$MVL, 0.05855459, tolerance = 1E-6)
 })
 
@@ -45,11 +45,11 @@ test_that("MVLgb estimates are historically consistent", {
   expect_equal(lac.wraw$raw, -0.05775767)
 })
 
-test_that("MVLc estimates are consitent for input side lengths or owin squares", {
+test_that("MVLc estimates are consistent for input side lengths or owin squares", {
   covar <- racscovariance(heather$coarse)
   p <- area(heather$coarse) / area(Frame(heather$coarse))
   sidelengths <- 2.2
-  lac <- lac(sidelengths, covar, p)
+  lac <- mvlc(sidelengths, covar, p)
   expect_equal(lac$MVL, mvlc(list(square(2.2)), covar, p))
 })
 
@@ -67,10 +67,10 @@ test_that("integration when covar is constant gives squared area", {
   covar <- as.im(owin(c(-6, 6), c(-6, 6)), eps = 0.01)
   p <- 1
   sidelengths <- seq(1, 2.2, by = 0.1)
-  lac <- lac(sidelengths, covar, p)
+  lac <- mvlc(sidelengths, covar, p)
   expect_equal(lac$MVL, rep(0, length(sidelengths)), tolerance = 0.01)
 
-  expect_equal(lac(lapply(c(0.5, 1, 2, 3), disc), covar, p), rep(0, 4), tolerance = 0.01)
+  expect_equal(mvlc(lapply(c(0.5, 1, 2, 3), disc), covar, p), rep(0, 4), tolerance = 0.01)
 })
 
 test_that("MVLc and MVLgb produce similar results for large square observation windows", {
