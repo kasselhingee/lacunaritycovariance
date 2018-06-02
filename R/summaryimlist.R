3b998cca4d562e6d0fe7ac120e6342c2a26b1292
Kassel Hingee <hinkass@gmail.com>
2018-06-02 18:27:32 +0800

    changed summary.imlist to have argument object

--- R/summaryimlist.R
+++ R/summaryimlist.R
@@ -1,34 +1,35 @@
 #' @title Pointwise summary of a list of im objects
 #' @export summary.imlist
+#' @method summary imlist
 #' @description
 #' This function assumes that im objects are each realisations of the same (stochastic) object. 
 #' It returns pointwise summaries such as observed sample mean and sample variance.
 
 #' @author Kassel Hingee
 
-#' @param  ims A list of im objects
-#' @param  harmonizeims If TRUE (default) the pixel dimensions of the images will be harmonized. Otherwise the ims will be tested for compatibility.
+#' @param  object A list of im objects
+#' @param  harmonizeobject If TRUE (default) the pixel dimensions of the images will be harmonized. Otherwise the object will be tested for compatibility.
 #' @param  ... Ignored
 #' @return A list im objects containing the pointwise mean, variance and maxima and minima.
 
 #' @examples
 #' obspatterns <- replicate(5, rbdd(10, 0.05, window = square(1)), simplify = FALSE)
-#' ims <- solapply(obspatterns, function(x) balancedracscovariances(x, obswin = square(1), modifications = "pickaH")[[1]])
-#' summ <- summary.imlist(ims, harmonizeims = FALSE)
+#' object <- solapply(obspatterns, function(x) balancedracscovariances(x, obswin = square(1), modifications = "pickaH")[[1]])
+#' summ <- summary.imlist(object, harmonizeobject = FALSE)
 
-summary.imlist <- function(ims, ...,  harmonizeims = TRUE){
-  if (harmonizeims) {ims <- do.call(harmonize.im, args = ims)} else { #for pointwise summaries the pixels must represent the same locations for each image.
-    stopifnot(do.call(compatible.im, args = ims))
+summary.imlist <- function(object, ...,  harmonizeobject = TRUE){
+  if (harmonizeobject) {object <- do.call(harmonize.im, args = object)} else { #for pointwise summaries the pixels must represent the same locations for each image.
+    stopifnot(do.call(compatible.im, args = object))
   }
-  #class(ims) <- "list"
-  n <- length(ims)
-  meanY <- Reduce(Add.im, ims)
+  #class(object) <- "list"
+  n <- length(object)
+  meanY <- Reduce(Add.im, object)
   meanY <- eval.im(meanY/n)
-  sumY2 <- Reduce(Add.im, lapply(ims, Square.im))
+  sumY2 <- Reduce(Add.im, lapply(object, Square.im))
   varY <- eval.im( (sumY2 - n *( meanY^2))/(n - 1))
   varY <- eval.im(pmax.int(0, varY))
-  maxY <- Reduce(Pmax.im, ims)
-  minY <- Reduce(Pmin.im, ims)
+  maxY <- Reduce(Pmax.im, object)
+  minY <- Reduce(Pmin.im, object)
   return(solist(mean = meanY, var = varY, max = maxY, min = minY))
 }
 
