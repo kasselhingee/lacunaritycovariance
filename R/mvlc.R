c1ec6091ad976939aae295ed24cd395e39de3542
Kassel Liam Hingee <kassel.hingee@research.uwa.edu.au>
2018-07-24 12:26:36 +0800

    shifted inner product to its own file

--- R/mvlc.R
+++ R/mvlc.R
@@ -102,38 +102,3 @@ mvlc.inputcovar <- function(boxes, covariance, p){
   ))
 }
 
-##  INNER PRODUCT FUNCTION ##
-#Parameters: A is an image, B is an image
-#outsideA and outsideB are the `value` of the image outside the image domain
-#na.ra are NA values skipped when summing up pixel values? (otherwise NA values stay as NA)
-innerprod.im <- function(A, B, outsideA = NA, outsideB = NA, na.rm = FALSE){
-  ##assume that NA is unknown but finite so that mutliplying 0*NA gives 0
-  integrationregion <- union.owin(Frame(A), Frame(B))
-  #check if results will be NA due to outsideA and outsideB
-  if (is.na(outsideA) && is.na(outsideB)){return(NA)}
-  # if one is NA outside and the other is non-zero outside then result is NA
-  if (is.na(outsideA) && (outsideB != 0)){return(NA)} 
-  if (is.na(outsideB) && (outsideA != 0)){return(NA)} 
-  #if one is non-zero and the other is non-zero then the result is Inf
-  if ((!is.na(outsideA)) && (outsideA != 0) && (outsideB != 0)) {return(Inf)}
-  if ((!is.na(outsideB)) && (outsideB != 0) && (outsideA != 0)) {return(Inf)}
- 
-  #outsideA is 0 or outsideB is 0 then ignore what happens outside the integration region 
-  if ((!is.na(outsideA)) && (outsideA == 0)){integrationregion <- intersect.owin(integrationregion, Frame(A))}
-  if ((!is.na(outsideB)) && (outsideB == 0)){integrationregion <- intersect.owin(integrationregion, Frame(B))}
-  
-  #now we have an integration region based on non-zero locations.
-  #we have that at least one must be non-NA outside
-  #we have that if one is NA and the other is non-zero outside then the result is NA
-  #we that if one is non-zero and the other is non-zero then the result is Inf
-  
-  harmgrid <- as.mask(integrationregion,
-             eps = c(min(A$xstep, B$xstep), min(A$ystep, B$ystep)))
-  A2 <- as.im(A, xy = harmgrid)
-  A2[setminus.owin(integrationregion, Frame(A))] <- outsideA
-  B2 <- as.im(B, xy = harmgrid)
-  B2[setminus.owin(integrationregion, Frame(B))] <- outsideB
-  prdimg <- eval.im(A2 * B2, harmonize = FALSE)
-  return(sum(prdimg[, ], na.rm = na.rm) * prdimg$xstep * prdimg$ystep)
-}
-
