234264abc9b7dca602c67cfc53983923c10a0c62
Kassel Liam Hingee <kassel.hingee@research.uwa.edu.au>
2018-07-31 18:43:41 +0800

    added scratch script for testing tests

---
+++ scriptfortestingtests.R
@@ -0,0 +1,19 @@
+library(devtools)
+library(profvis)
+library(testthat)
+
+test_check()
+getwd()
+
+profout <- profvis(test("."), interval = 0.1)
+#on 31 July the above took 143900ms = 2.4 minutes on my uwa computer. After reducing rbdr resolution the time is down to 70 seconds
+
+system.time(test("."))
+
+
+#running specific tests
+profout <- profvis(test(".", filter = "imbinaryops"), interval = 0.1)
+htmlwidgets::saveWidget(profout, "testprofile.html")
+
+
+test(".", filter = "rbdr")
