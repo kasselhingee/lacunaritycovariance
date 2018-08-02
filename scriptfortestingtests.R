f10b32a4ec5bf043ca1d3ca902e55097d8fd1043
Kassel Liam Hingee <kassel.hingee@research.uwa.edu.au>
2018-08-02 13:09:52 +0800

    small notes about scripting for tests

--- scriptfortestingtests.R
+++ scriptfortestingtests.R
@@ -2,20 +2,23 @@ library(devtools)
 library(profvis)
 library(testthat)
 
+library(devtools)
+install(upgrade_dependencies = FALSE)
+install(quick = TRUE, upgrade_dependencies = FALSE)
+
 test_check()
 getwd()
 
+#timing of testing
 profout <- profvis(test("."), interval = 0.1)
-#on 31 July the above took 143900ms = 2.4 minutes on my uwa computer. After reducing rbdr resolution the time is down to 70 seconds
+#on 31 July the above took 143900ms = 2.4 minutes on my uwa computer.
+#After reducing rbdr resolution the time is down to 70 seconds
 
 system.time(test("."))
-
 
 #running specific tests
 profout <- profvis(test(".", filter = "imbinaryops"), interval = 0.1)
 htmlwidgets::saveWidget(profout, "testprofile.html")
-
-
 test(".", filter = "rbdr")
 
 #test plotting with vdiffr:
