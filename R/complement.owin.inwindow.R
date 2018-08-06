c96e12eb7b00088f9177759eef381f43973abe79
Kassel Liam Hingee <kassel.hingee@research.uwa.edu.au>
2018-08-06 17:33:06 +0800

    complementowinwindow test added

--- R/complement.owin.inwindow.R
+++ R/complement.owin.inwindow.R
@@ -18,18 +18,6 @@
 #' xi <- heather$coarse
 #' xic <- complement.owin.inwindow(xi, Frame(heather$coarse))
 
-#tests
-# library(testthat)
-# w <- disc(radius = 5, centre = c(5,10))
-# xi <- heather$coarse
-# xic <- complement.owin.inwindow(xi, w)
-# expect_equal(area.owin(union.owin(intersect.owin(xi, w), xic)), area.owin(w), tolerance = area.owin(w)/1000)
-# expect_equal(area.owin(intersect.owin(xi, xic)), 0)
-# xip <- as.polygonal(xi)
-# xipc <- complement.owin.inwindow(xip, w)
-# expect_equal(area.owin(union.owin(intersect.owin(xip, w), xipc)), area.owin(w), tolerance = area.owin(w)/1000)
-# expect_equal(area.owin(intersect.owin(xip, xipc)), 0, tolerance = area.owin(w)/1E6)
-# xic <- complement.owin.inwindow(square(), square(r=0.5))
 
 #' @details 
 #' The usual complement.owin function in spatstat does not (to my knowledge) accomplish the task that complement.owin.inwindow does as
