f2588eda414ccc5fed4776d1d56f476e409f66f5
Kassel Liam Hingee <kassel.hingee@research.uwa.edu.au>
2018-08-21 12:21:37 +0800

    tradcovarest help updated

--- R/tradcovarest.R
+++ R/tradcovarest.R
@@ -1,22 +1,22 @@
-#' @title A spatial covariance, also known as `two-point probability', estimator for stationary RACS
+#' @title Traditional covariance estimator
 #' @export tradcovarest
 #' @description 
-#' This function estimate the covariance of a stationary RACS. 
-#' The covariance is also known as the two-point coverage probability, and very closely related to the semivariogram.
-#'  The covariance of a vector \eqn{v} is the probability of two points separated by a vector \eqn{v} being covered by the random set \eqn{\Xi}
-#' \deqn{C(v) = P(\{x,x+v\}\subseteq \Xi).}
+#' This function computes the traditional covarience estimate of a stationary RACS from a binary map.
+#' For a stationary RACS, \eqn{\Xi}, the covariance 
+#' for a vector \eqn{v} is the probability of two points separated by a vector \eqn{v} are covered by \eqn{\Xi}
+#' \deqn{C(v) = P(\{x,x+v\}\subseteq \Xi).}{C(v) = P({x, x+ v} in Xi).}
 #' @author{Kassel Liam Hingee}
 
 
 
-#' @param xi An observation of the RACS of interest. It can be in \pkg{spatstat}'s \code{owin} or \code{im} format. If \code{xi} is in \code{im} format then it is assumed that the pixels will be valued 1 (for foreground), 0 (for background) and NA for unobserved.
-#' If \code{xi} is in \code{owin} format take care to consider what exactly the observation window is (if none is supplied then it will be assumed that the observation window is the smallest rectangle enclosing \code{xi}).
-#' @param obswin The observation window in \code{owin} format. If it isn't included and \code{xi} is an \code{owin} object then \code{obswin} is taken to be the smallest rectangle enclosing \code{xi}. If \code{xi} is a \code{im} object than \code{obswin} is all the non-NA pixels in \code{xi}.
-#' @param setcov_boundarythresh Any vector \eqn{v} such that set covariance of the observation window is smaller than this threshold is given a covariance of NA to avoid instabilities caused by dividing by very small areas.
-#' If NULL is supplied (default) then 0.1 * area.owin(obswin) is used
-#' @param inclraw If TRUE the output will be two \code{im} objects one for the standard estimator and one raw estimate.
+#' @param xi A binary map of an observation of a RACS of interest. See
+#'   \code{\link{stationaryracsinference-package}} for details.
+#' @param obswin If \code{xi} is an \code{owin} object then \code{obswin} is an
+#'   \code{owin} object that specifies the observation window.
+#' @param setcov_boundarythresh Any vector \eqn{v} such that set covariance of the observation window
+#'  is smaller than this threshold is given a covariance of NA to avoid instabilities caused by dividing by very small areas, 
 
-#' @return A \pkg{SpatStat} \code{im} object containing the estimated covariance. The grey scale values in this image represent the covariance for an array of vectors. If the raw version is requested then a list of \code{im} objects is returned.
+#' @return A \pkg{SpatStat} \code{im} object containing the estimated covariance.
 
 
 #' @examples
@@ -27,14 +27,16 @@
 #' @keywords spatial nonparametric
 
 #' @details 
-#' The reduced sample estimator [1] is
-#' \deqn{ \hat{C}(v) = \frac{\gamma_{W\cap X}(v)}{\gamma_W(v)}}
-#' where \eqn{\gamma_{W}(v)} is the set covariance of the observation window \eqn{|W \cap (W\oplus v)|} and \eqn{\gamma_{W\cap X}(v)} is the set covariance of an observation, \eqn{X}, of the RACS \eqn{\Xi}, 
-#' \eqn{\gamma_{W\cap X}(v) = |W \cap X \cap ((W\cap X ) \oplus v)|}.
-#' The raw estimate (if requested) is
-#' \deqn{\hat{\tilde{C}}(v) = \frac{\gamma_{W\cap X}(v)}{|W|}.}
-#' \code{tradcovarest} uses Fourier transforms to calculate set covariances (using the \code{\link{setcov}} function from \pkg{spatstat}). 
-#' Vectors with small \eqn{\gamma_W(v)} are eliminated using \code{setcov_boundarythresh} because they cause numerical instabilities.
+#' The traditional covariance estimator is [1]
+#' \deqn{ \hat{C}(v) = \frac{\gamma_{W\cap X}(v)}{\gamma_W(v)}}{ C(v) = gammaWX(v) / gammaW(v) }
+#' where \eqn{\gamma_{W}(v)}{gammaW(v)} is the set covariance of the observation window \eqn{W} 
+#' and \eqn{\gamma_{W\cap X}(v)}{gammaWX(v)} is the set covariance of the foreground within \eqn{W}.
+
+#' \code{tradcovarest} uses Fourier transforms to calculate the set covariances (using the \code{\link[spatstat]{setcov}} function from \pkg{spatstat}). 
+#' Vectors with small \eqn{\gamma_W(v)}{ gammaW(v) } are eliminated using \code{setcov_boundarythresh} 
+#' as division by small values is numerically unstable.
+#' 
+#' **require obswin if xi is owin.
 # I suspect this instabilities are because the fourier transforms are only approximately correct, and 0 is within the approximately correct range.
 
 #' @references [1] Serra, J.P. (1982) Image Analysis and Mathematical Morphology. London; New York: Academic Press.
