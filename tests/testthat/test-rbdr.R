f914f04e4b8ab72aaf6e2200fcda49b44a7840df
Kassel Liam Hingee <kassel.hingee@research.uwa.edu.au>
2019-05-16 20:47:19 +0900

    made testing of NOT_CRAN more in line with skip_on_cran function, I think this removed some bugs

--- tests/testthat/test-rbdr.R
+++ tests/testthat/test-rbdr.R
@@ -2,9 +2,9 @@ context("RACS simulation")
 
 test_that("rbdr produces simulations with the correct coverage and covariance", {
   grain <- owin(xrange = c(-5, 5), yrange = c(-5, 5))
-  win <- owin(xrange = c(0, 200), c(0, 200))
-  if (Sys.getenv("NOT_CRAN") == "false"){
-    win <- owin(xrange = c(0, 20), c(0, 20))
+  win <- owin(xrange = c(0, 20), c(0, 20))
+  if (identical(Sys.getenv("NOT_CRAN"), "true")){
+    win <- owin(xrange = c(0, 200), c(0, 200))
   }
   lambda <- 4.2064E-3
   
